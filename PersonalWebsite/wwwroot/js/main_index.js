01e5732d31c823d86798cf6af49646b1be404d29
Niels Pressel <niels.pressel@gmx.de>
2020-03-31 12:44:39 +0200

    Initial commit

---
+++ PersonalWebsite/wwwroot/js/main_index.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(() => {
+    $('#main-nav').removeClass('box-shadow');
+    $('#main-nav').removeClass('border-bottom');
+});
+
+
+$(window).scroll(() => {
+
+    if ($(window).scrollTop() >= 50) {
+        $('#main-nav').addClass('active');
+        $('#main-nav').addClass('box-shadow');
+        $('#main-nav').addClass('border-bottom');
+    }
+    else {
+        $('#main-nav').removeClass('active');
+        $('#main-nav').removeClass('box-shadow');
+        $('#main-nav').removeClass('border-bottom');
+    }
+});
