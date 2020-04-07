1d4c5ec04431106570446f34c1cf2376ebe7230d
Niels Pressel <niels.pressel@gmx.de>
2020-04-07 12:24:45 +0200

    Added monaco editor to the edit page to directly enter html or markdown there with some decent syntax highlighting and code completion

---
+++ PersonalWebsite/wwwroot/js/main_project_edit.js
@@ -0,0 +1,46 @@
+﻿"use strict";
+var el = document.getElementById('editor');
+el.style.height = '500px';
+
+// window.editor is accessible.
+var editor = null;
+var init = function () {
+
+    require(['vs/editor/editor.main'], function () {
+
+        editor = monaco.editor.create(el, {
+            language: 'html'
+        });
+
+        editor.layout();
+    });
+
+    // no point in keeping this around.
+    window.removeEventListener("load", init);
+
+    var type = Number.parseInt($('#text-format-select').val());
+    if (type === 0) {
+        monaco.editor.setModelLanguage(editor.getModel(), 'markdown');
+    }
+    else if (type === 1) {
+        monaco.editor.setModelLanguage(editor.getModel(), 'html');
+    }
+
+    editor.setValue($('#Data_CustomExperience').val());
+};
+
+function OnLanguageChange(value) {
+    value = Number.parseInt(value);
+    if (value === 0) {
+        monaco.editor.setModelLanguage(editor.getModel(), 'markdown');
+    }
+    else if (value === 1) {
+        monaco.editor.setModelLanguage(editor.getModel(), 'html');
+    }
+}
+
+$('#edit-form').submit(function (e) {
+    $('#Data_CustomExperience').val(editor.getValue());
+});
+
+window.addEventListener("load", init);
